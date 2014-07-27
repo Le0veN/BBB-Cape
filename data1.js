525980a36396d88255d12aed44cac9ae8928d41e
van Hohenheim <alex.aungurencei@gmail.com>
2014-07-27 15:45:30 +0300

    Create data1.js

---
+++ data1.js
@@ -0,0 +1,285 @@
+# cat EPROM.json                       
+{
+ "Display-Cape.eeprom": {
+    "type": "cape",
+    "header": "aa5533ee",
+    "formatRev": "A0",
+    "boardName": "4.3inch Newhaven Display Cape",
+    "version": "01",
+    "manufacturer": "AlexAungurencei",
+    "partNumber": "000100-001",
+    "numPins": 53,
+    "serialNumber": "3014AAGC0001",
+    "currentVDD_3V3EXP": 250,
+    "currentVDD_5V": 250,
+    "currentSYS_5V": 0,
+    "DCSupplied": 1000,
+    "mux": {
+      "P8_20": {
+        "used": "used",
+        "direction": "in",
+        "pullup": "pullup",
+		"slew": "fast"
+        "rx": "disabled",
+        "mode": 7,
+        "function": "gpio1_31"
+      },
+	  "P8_22": {
+        "used": "used",
+        "direction": "in",
+        "pullup": "pullup",
+		"slew": "fast"
+        "rx": "disabled",
+        "mode": 7,
+        "function": "gpio1_5"
+      },
+	  "P8_24": {
+        "used": "used",
+        "direction": "in",
+        "pullup": "pullup",
+		"slew": "fast"
+        "rx": "disabled",
+        "mode": 7,
+        "function": "gpio1_1"
+      },
+	  "P8_26": {
+        "used": "used",
+        "direction": "in",
+        "pullup": "pullup",
+		"slew": "fast"
+        "rx": "disabled",
+        "mode": 7,
+        "function": "gpio1_29"
+      },
+      "P8_27": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_vsync"
+      },
+      "P8_28": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_pclk"
+      },
+      "P8_29": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_hsync"
+      },
+      "P8_30": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_ac_bias_en"
+      },
+	  "P8_31": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_data14"
+      },
+      "P8_32": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_data15"
+      },
+      "P8_33": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_data13"
+      },
+      "P8_34": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_data11"
+      },
+      "P8_35": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_data12"
+      },
+	 "P8_36": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_data10"
+      },
+      "P8_37": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_data8"
+      },
+      "P8_38": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_data9"
+      },
+      "P8_39": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_data6"
+      },
+	  "P8_40": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_data7"
+      },
+      "P8_41": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_data4"
+      },
+      "P8_42": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_data5"
+      },
+      "P8_43": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_data2"
+      },
+	  "P8_44": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_data3"
+      },
+      "P8_45": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_data0"
+      },
+      "P8_46": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 0,
+        "function": "lcd_data1"
+      },
+	  "P9_14": {
+        "used": "used",
+        "direction": "out",
+        "pullup": "disabled",
+        "mode": 6,
+        "function": "ehrpwm1a"
+      },
+	  "P9_19": {
+        "used": "used",
+        "direction": "bidir",
+		"slew": "fast"
+        "rx": "disabled",
+        "mode": 3,
+        "function": "I2C2_SCL"
+      },
+	  "P9_20": {
+        "used": "used",
+        "direction": "bidir",
+		"slew": "fast"
+        "rx": "disabled",
+        "mode": 3,
+        "function": "I2C2_SDA"
+      },
+	  "P9_25": {
+        "used": "used",
+        "direction": "in",
+		"slew": "fast"
+        "rx": "disabled",
+        "mode": 7,
+        "function": "gpio3_21"
+      },
+	  "P9_27": {
+        "used": "used",
+        "direction": "out",
+		"pullup": "disabled",
+		"slew": "slow"
+        "rx": "disabled",
+        "mode": 7,
+        "function": "gpio3_19"
+      },
+	  "P9_28": {
+        "used": "used",
+        "direction": "out",
+		"pullup": "disabled",
+		"slew": "fast"
+        "rx": "disabled",
+        "mode": 7,
+        "function": "gpio3_17"
+      },
+	  "P9_29": {
+        "used": "used",
+        "direction": "bidir",
+		"slew": "fast"
+        "rx": "disabled",
+        "mode": 3,
+        "function": "spi1_d0"
+      },
+	  "P9_31": {
+        "used": "used",
+        "direction": "bidir",
+		"slew": "fast"
+        "rx": "disabled",
+        "mode": 3,
+        "function": "spi1_sclk"
+      },
+	  "P9_33": {
+        "used": "used",
+        "function": "AIN4"
+      },
+	  "P9_35": {
+        "used": "used",
+        "function": "AIN6"
+      },
+	  "P9_37": {
+        "used": "used",
+        "function": "AIN2"
+      },
+	  "P9_39": {
+        "used": "used",
+        "function": "AIN0"
+      },
+	  "P9_41": {
+        "used": "used",
+        "direction": "bidir",
+		"slew": "fast"
+        "rx": "disabled",
+        "mode": 3,
+        "function": "clkout2"
+      }
+    }
+  }
+}
